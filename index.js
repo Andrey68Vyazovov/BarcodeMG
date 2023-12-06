e0190d7f3eb8c734308f72cf1c0090cddfa066fa
Andrey Vyazovov <andreyy-vjazovov@rambler.ru>
2023-12-06 15:47:20 +0300

    ver0.42

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inputButton = document.querySelector(".popup__close");
 const inputForm = document.querySelector(".input_1"); 
 const inputCounter = document.querySelector(".counter"); 
 const arrayBarcode = [];
+const emailBodyText='';
+const subjectText='';
 let counter_1=0;
 
 
@@ -17,6 +19,9 @@ inputForm.addEventListener('input', ()=>{
 }); 
 
 inputButton.addEventListener('click', ()=>{
+emailBodyText= arrayBarcode;
+subjectText='transit-barcode:';
+console.log(subjectText);
 console.log(arrayBarcode);
 counter_1=0;
 inputCounter.textContent='Отсканировано ШК: '+counter_1;
@@ -26,8 +31,8 @@ arrayBarcode.length=0;
 
 function sendEmail() {
   const email = 'vyazovov_av@magnit.ru'; //rao_rctm@tambovrc.magnit.ru
-  const subject = 'transit-barcode:'+ now;
-  const emailBody = arrayBarcode.join('\r\n');
+  const subject = subjectText;
+  const emailBody = emailBodyText;
   document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody;
 }
 
