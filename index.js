8a42df7c75128021e322aeb109f002fa517ce49b
Andrey Vyazovov <andreyy-vjazovov@rambler.ru>
2023-12-08 16:20:59 +0300

    ver:0.14

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const inputButton = document.querySelector(".popup__close");
 const inputForm = document.querySelector(".input_1"); 
 const inputCounter = document.querySelector(".counter");
 const rlButton = document.querySelector(".button_reload"); 
+const tgButton = document.querySelector(".button_toggle");
 const selects = document.querySelector("select");
 const img = document.querySelector(".svg_img");
+const img_tg = document.querySelector(".svg_toggle");
 let arrayBarcode = [];
 let emailBodyText='';
 let subjectText='';
@@ -36,16 +38,23 @@ arrayBarcode.length=0;
 
 rlButton.addEventListener('click', () => {  
   setTimeout(() => { 
-    img.classList.add('svg_img_rotate');  
-  },300);  
+   img.classList.add('svg_img_rotate');  
+  },300);
   setTimeout(() => { 
     img.classList.remove('svg_img_rotate'); 
-    location.reload();
+  },1200);
+  setTimeout(() => { 
+    location.reload(); 
+  },1600);
+}); 
+
+tgButton.addEventListener('click', () => {  
+  setTimeout(() => { 
+    img_tg.classList.add('svg_toggle_rotate');  
+  },200);  
+  setTimeout(() => { 
+    img_tg.classList.remove('svg_toggle_rotate'); 
   },1000);
-  
- 
-//setTimeout(() => { 
-  //location.reload();},1000);
   }); 
 
 function sendEmail() {
