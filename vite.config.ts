5d192e704b4cc239e85aa16a3addd2a060914c38
Andrey68Vyazovov <139168951+Andrey68Vyazovov@users.noreply.github.com>
2025-11-18 16:48:44 +0300

    fix: delpoy flow 4 vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/BarcodeMG/',
-  root: '.', // Корень проекта
-  publicDir: 'public', // Папка со статическими файлами
+  root: '.',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: './public/index.html' // Явно указываем путь к index.html
-    }
+    emptyOutDir: true
   }
 })
